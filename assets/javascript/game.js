5d917c6bcb43d27901b70c89653a0bd8d6343546
Tamar Auber <mischegoss@gmail.com>
2019-01-31 16:19:29 -0500

    Add word splitter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,48 @@
 let pickedName = "actor"
 let pickedImage = "assets/images/" + pickedName + ".png"
+let blankWord = []
+let pickedNameLength = pickedName.length
+let pickedNameLetters = []
+let wordblanks = document.getElementById("guess-word")
 
-console.log(pickedImage)
+let userpick = "r";
+
 function imageChanger()  {
     image =document.getElementById("changingImage")
     image.src = pickedImage
 
 }
 
+
+function splitPickedName () {
+    for (var i=0; i <pickedNameLength; i++) {
+      pickedNameLetters.push(pickedName.charAt(i));
+    }
+    if (pickedNameLetters.includes(userpick)) {
+        index = pickedNameLetters.findIndex(pickedNameLetter => pickedNameLetter === userpick);
+        console.log(index)
+    } else {
+        console.log("No Match")
+    }
+}
+
+
+
+function buildOurBlank() {
+    var blankbuilt = "";
+    for(var i = 0; i < blankWord.length; i++){
+        blankbuilt += " "+blankWord[i];
+    }
+    return blankbuilt; 
+}
+
+function blankSetter() {
+for (var i = 0; i < pickedNameLength; i++) {
+    blankWord.push("_");}  
+  /* wordblanks = document.getElementById("guess-word") */
+   wordblanks.innerHTML = buildOurBlank()
+}
+
 window.onload = imageChanger()
+window.onload = blankSetter()
+window.onload = splitPickedName()
